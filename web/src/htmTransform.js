59563ffc198843f3a26cf8bf33458044b1dc550e
Luiz Américo <luizamericop@gmail.com>
2019-04-27 21:47:17 -0300

    web: add prettier support

--- web/src/htmTransform.js
+++ web/src/htmTransform.js
@@ -1,5 +1,8 @@
 import { compile as hbsToJSX } from 'handlebars-to-jsx'
 import { transform, registerPlugin } from '@babel/standalone'
+import prettier from 'prettier/standalone'
+import prettierBabel from 'prettier/parser-babylon'
+import prettierHtml from 'prettier/parser-html'
 import jsxToHtmBabelPlugin from '../../babel-plugin-transform-jsx-to-htm'
 
 registerPlugin('jsx-to-htm', jsxToHtmBabelPlugin)
@@ -10,12 +13,12 @@ export const htmTransform = (src, options = {}) => {
   const jsx = sourceType === 'handlebars' ? hbsToJSX(src, hbsToJSXOptions) : src
 
   if (toJSX) {
-    return jsx
+    return prettier.format(jsx, {parser: 'babylon', plugins: [prettierBabel]})
   }
 
   const { code } = transform(jsx, {
     plugins: ['jsx-to-htm']
   })
 
-  return code  
+  return prettier.format(code, {parser: 'babylon', plugins: [prettierBabel, prettierHtml]})
 }
